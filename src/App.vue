813d397685b0492df26ea69f94e1313a250a1e4c
陈小龙 <13480731740@qq.com>
2017-01-04 17:52:30 +0800

    布局方法

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+  <div id=app>
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">
+        <a>商品</a>
+      </div>
+      <div class="tab-item">
+        <a>评论</a>
+      </div>
+      <div class="tab-item">
+        <a>商家</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import header from './components/header/header'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    'v-header':header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.tab{
+    display: flex;
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
+}
+.tab-item{
+    flex: 1;
+    text-align: center;
 }
 </style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        我是头部
+    </div>
+</template>
+
+<script>
+    export default{}
+</script>
+
+<style>
+</style>
