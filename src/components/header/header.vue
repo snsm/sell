ad7bd4af0cc90147f410ff3bacd2764a27f7d026
陈小龙 <13480731740@qq.com>
2017-01-05 14:55:50 +0800

    详情弹出页(1) 实现弹出层

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,18 +17,19 @@
                     <span class="text">{{ seller.supports[0].description }}</span>
                 </div>
             </div>
-            <div v-if="seller.supports" class="support-count">
+            <div v-if="seller.supports" class="support-count" @click="showDetail">
                 <span class="count" >{{ seller.supports.length }} 个</span>
                 <i class="icon-keyboard_arrow_right">></i>
             </div>
         </div>
-        <div class="bulletin-wrapper">
+        <div class="bulletin-wrapper" @click="showDetail">
             <span class="bulletin-title"></span><span class="bulletin-text">{{ seller.bulletin }}</span>
             <i class="icon-keyboard_arrow_rights">></i>
         </div>
         <div class="background">
             <img :src="seller.avatar" width="100%" height="100%">
         </div>
+        <div v-show="detailShow" class="detail"></div>
     </div>
 </template>
 
@@ -39,6 +40,16 @@
             type: Object
           }
         },
+        data(){
+            return {
+                detailShow: false
+            };
+        },
+        methods: {
+            showDetail() {
+                this.detailShow = true;
+            }
+        }
     }
 </script>
 
@@ -47,6 +58,7 @@
         position: relative;
         color:#fff;
         background: rgba(7,17,27,0.5);
+        overflow: hidden;
     }
     .background{
         position: absolute;
@@ -164,5 +176,16 @@
         right: 12px;
         top: 2px;
     }
+
+   .detail{
+    position: fixed;
+    z-index: 100;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background: rgba(7,17,27,0.8);
+   }
 
 </style>
