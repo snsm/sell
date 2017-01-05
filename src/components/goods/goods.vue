1caf14c143eba8910fc83792f5254d121a8e41d9
陈小龙 <13480731740@qq.com>
2017-01-05 21:41:08 +0800

    右侧食品列表布局 2

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -12,14 +12,14 @@
     <div class="foods-wrapper">
         <ul>
             <li v-for="item in goods" class="food-list">
-                <h1 class="title">{{ item.name }}</h1>
+                <h1 class="titles">{{ item.name }}</h1>
                 <ul>
                     <li v-for="food in item.foods" class="food-item">
-                        <div class="icon">
-                            <img :src="food.icon" >
+                        <div class="icons">
+                            <img :src="food.icon" width="57" height="57" >
                         </div>
-                        <div class="content">
-                            <h2 class="name">{{ food.name }}</h2>
+                        <div class="contents">
+                            <h2 class="names">{{ food.name }}</h2>
                             <p class="desc">{{ food.description }}</p>
                             <div class="extra">
                                 <span class="count">月售{{ food.sellCount }}份</span>
@@ -109,5 +109,43 @@
         border-bottom-style: solid;
         border-bottom-width: 1px;
         border-bottom-color: gainsboro;
+    }
+    .foods-wrapper{
+        flex: 1px;
+    }
+    .titles{
+        padding-left: 14px;
+        height: 26px;
+        lien-height: 26px;
+        border-left: 2px solid #d99de1;
+        font-size: 12px;
+        color: rgb(147,153,159);
+        background: #f3f5f7;
+    }
+    .food-item{
+        display: flex;
+        margin: 18px;
+        padding-bottom: 18px;
+        border-bottom-style: solid;
+        border-bottom-width: 1px;
+        border-bottom-color: gainsboro;
+    }
+    .food-item:nth-child{
+        border: none;
+        margin-bottom: 0px;
+    }
+    .icons{
+        flex: 0 0 57px;
+        margin-right: 10px;
+    }
+    .contents{
+        flex: 1px;
+    }
+    .names{
+        margin: 2px 0 8px ;
+        height: 14px;
+        line-height: 14px;
+        font-size: 14px;
+        color: rgb(7,17,27);
     }
 </style>
