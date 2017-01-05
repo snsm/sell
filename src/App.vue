d51e0ef21cd6b50e50fb5fa6bedf8eb1652fb5bf
陈小龙 <13480731740@qq.com>
2017-01-05 11:34:00 +0800

    外部主键（2）添加样式，采用style写，不是STYLUS写的

--- src/App.vue
+++ src/App.vue
@@ -46,6 +46,7 @@ export default {
 </script>
 
 <style>
+
 .tab{
     display: flex;
     width: 100%;
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -39,4 +39,42 @@
         color:#fff;
         background-color:black;
     }
+    .content-wrapper{
+        padding: 24px 12px 18px 24px;
+        font-size: 0;
+    }
+    .avatar{
+        display: inline-block;
+        vertical-align: top;
+    }
+    .avatar img{
+        border-radius: 2px;
+    }
+    .content{
+        display: inline-block;
+        margin-left: 16px;
+    }
+    .title{
+        margin: 2px 0 8px 0;
+    }
+    .brand{
+        display: inline-block;
+        vertical-align: top;
+        width: 30px;
+        height: 18px;
+        background-image: url("brand@2x.png");
+        background-size: 30px 18px;
+        background-repeat: no-repeat;
+    }
+    .name{
+        margin-left: 6px;
+        font-size: 16px;
+        line-height: 18px;
+        font-weight: bold;
+    }
+    .description{
+        margin-bottom: 10px;
+        line-height: 12px;
+        font-size: 12px;
+    }
 </style>
