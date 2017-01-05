acd9de6018fd67c322091f9aa729f516fbaf7432
陈小龙 <13480731740@qq.com>
2017-01-05 09:34:46 +0800

    安装vue-resource 请求接口数据

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <v-header></v-header>
-    <div class="tab border-1px">
+    <div class="tab">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
       </div>
@@ -17,13 +17,28 @@
 </template>
 
 <script>
-
 import header from './components/header/header'
 import goods from 'components/goods/goods'
 import geller from 'components/seller/seller'
 import ratings from 'components/ratings/ratings'
 
+const ERR_OK = 0;
+
 export default {
+  data () {
+    return {
+      seller: {}
+    };
+  },
+  created() {
+      this.$http.get('/api/seller').then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.seller = response.data;
+          console.log(this.seller);
+        }
+      });
+    },
   components: {
     'v-header':header
   },
@@ -36,7 +51,9 @@ export default {
     width: 100%;
     height: 40px;
     line-height: 40px;
-    position: relative;
+    border-bottom-style: solid;
+    border-bottom-width: 1px;
+    border-bottom-color: gainsboro;
 }
 .tab-item{
     flex: 1;
@@ -47,11 +64,10 @@ export default {
 .tab .tab-item > a {
   display: block;
   font-size: 14px;
-  color: rgb(77, 85, 93);
+  color: #4d555d;
 }
 
 .tab .tab-item > a.active {
-  color: rgb(240, 20, 20)
+  color: #f01414;
 }
-
 </style>
