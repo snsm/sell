15085d75aa1b3b0a97f368c85852e2f6682cb150
陈小龙 <13480731740@qq.com>
2017-01-05 16:11:35 +0800

    star 组件未完成

---
+++ src/components/star/star.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="star" :class="starType">
+        <span v-for="itemClass in itemClasses" :class="itemClass" class="star-item"></span>
+    </div>
+</template>
+
+<script>
+    export default{
+        props: {
+            size: {
+                type: Number
+            },
+            score: {
+                type: Number
+            },
+        },
+        computed: {
+            starType(){
+                return 'star-' + this.size;
+            }
+        },
+    }
+</script>
+
+<style>
+    .star{
+        font-size: 0;
+    }
+    star-item{
+        display: inline-block;
+        background-repeat: no-repeat;
+    }
+    .star.star-48 .star-item.on {
+    background-image: url("star24_half@2x.png");
+}
+    .star.star-48 .star-item {
+        width: 20px;
+        height: 20px;
+        margin-right: 22px;
+        background-size: 20px 20px;
+}
+.star .star-item {
+    display: inline-block;
+    background-repeat: no-repeat;
+}
+</style>
